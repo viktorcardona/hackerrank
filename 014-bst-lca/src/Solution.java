d2d70a989804fe010683d340a23ef0df1a7d41fe
Victor Cardona <victorcardona@Victors-MacBook-Air.local>
2018-11-06 22:07:47 -0500

    BST: Binary Search Tree: LCA: Lowest Common Ancestor

---
+++ 014-bst-lca/src/Solution.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ *  Binary Search Tree : Lowest Common Ancestor
+ *
+ *  https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor/problem
+ *
+ *
+ */
+public class Solution {
+
+    public static Node lca(Node root, int v1, int v2) {
+        // Write your code here.
+        if (root.data == v1 || root.data == v2) return root;
+        if (v1 < root.data && v2 > root.data) return root;
+        if (v2 < root.data && v1 > root.data) return root;
+        if (v1 < root.data && v2 < root.data) return lca(root.left, v1, v2);
+        return lca(root.right, v1, v2);
+    }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        int v1 = scan.nextInt();
+        int v2 = scan.nextInt();
+        scan.close();
+        Node ans = lca(root,v1,v2);
+        System.out.println(ans.data);
+    }
+
+}
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
